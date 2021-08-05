71ea1bb20127de4ea698205a1f20fceaf8706260
Jixuan Cao <jixuan.cao@mebank.com.au>
2021-08-05 13:43:34 +1000

    Fix the missing manifest issue and add modified README

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,3 +22,16 @@ tasks.test {
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "11"
 }
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "com.codingchallenge.transactionanalyser.TransactionAnalyserAppKt"
+    }
+
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+
+    // This line of code recursively collects and copies all of a project's files
+    // and adds them to the JAR itself. One can extend this task, to skip certain
+    // files or particular types at will
+    from(configurations.compileClasspath.map { config -> config.map { if (it.isDirectory) it else zipTree(it) } })
+}
